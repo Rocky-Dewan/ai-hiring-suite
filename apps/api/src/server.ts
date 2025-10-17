ddd0acf0e70f7ea0cac42e940fd6c7224f185063
Rocky Dewan <76462396+Rocky-Dewan@users.noreply.github.com>
2025-10-18 02:21:37 +0600

    Update server.ts

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,6 +1,8 @@
+import dotenv from 'dotenv'
+import 'dotenv/config'
 import http from 'http'
 import app from './app'
-import dotenv from 'dotenv'
+
 import path from 'path'
 
 dotenv.config({ path: path.resolve(process.cwd(), '../../.env') })
